fb14ecfd30945e1b4d030a0576def1c921e9d8f7
Seyves <igor.906090@gmail.com>
2026-06-28 16:04:49 +0300

    added favicon

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -31,13 +31,27 @@ export const Route = createRootRouteWithContext()({
                 content: 'width=device-width, initial-scale=1',
             },
             {
-                title: 'TanStack Start Starter',
+                title: 'TwinSpeak',
             },
         ],
         links: [
             {
                 rel: 'stylesheet',
                 href: appCss,
+            },
+            {
+                rel: 'icon',
+                type: 'image/svg+xml',
+                href: '/favicon.svg',
+            },
+            {
+                rel: 'icon',
+                type: 'image/x-icon',
+                href: '/favicon.ico',
+            },
+            {
+                rel: 'apple-touch-icon',
+                href: '/apple-touch-icon.png',
             },
         ],
     }),
